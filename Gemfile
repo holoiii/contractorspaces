13ac8cc175c0481115ab024ccc19057e3d0fe267
Albert Peng <albert.peng@cureus.com>
2014-02-04 21:18:55 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'haml'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'thin'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contractorspaces::Application.config.secret_key_base = 'bd26b87b207fd604d47b2c4ae27874040d129866361c0cb301fe5663def0a10dbd064c5b262b48c48e8a615e5d643167506e8f24bc07128d1f0f36afcf56c6b8'
